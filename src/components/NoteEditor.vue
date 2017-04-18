35b32e0364fe38d7789081511327c94d35a47c21
JoaoFCosta <dinojoaocosta@gmail.com>
2017-04-18 19:26:09 +0100

    Changes in notes are now persisted to Firebase.

--- src/components/NoteEditor.vue
+++ src/components/NoteEditor.vue
@@ -9,24 +9,23 @@
         a.nav-item.is-pulled-right
           button.button.is-outlined(@click='$router.push("/#")')
             i.fa.fa-fw.fa-arrow-left
-          button.button.is-danger
+          button.button.is-danger(@click='destroyNote')
             i.fa.fa-fw.fa-trash
-          button.button.is-primary(@click='editing = !editing')
+          button.button.is-primary(@click='saveNote')
             i.fa.fa-fw.fa-pencil(v-show='!editing')
             i.fa.fa-fw.fa-check(v-show='editing')
   hr
   br
   button.button.is-loading.is-fullwidth(v-show='note.timestamp == null', style='border: 0')
-  p(v-show='!editing') {{ note.text }}
+  .note-text(v-show='!editing', v-html='mdnote')
   textarea(v-show='editing', v-model='note.text')
 </template>
 
 <script>
 import firebase from 'firebase'
 import $ from 'jquery'
+import marked from 'marked'
 import 'autogrow'
-
-$('textarea').autogrow()
 
 export default {
   name: 'note-editor',
@@ -38,16 +37,38 @@ export default {
   firebase () {
     return {
       note: {
-        source: firebase.database().ref('notes/' + this.$route.params.id),
+        source: firebase.database().ref('notes').child(this.$route.params.id),
         asObject: true
       }
+    }
+  },
+  computed: {
+    mdnote () {
+      return marked(this.note.text)
+    }
+  },
+  methods: {
+    saveNote () {
+      this.editing = !this.editing
+      $('textarea').autogrow()
+      $('textarea').focus()
+      if (!this.editing) {
+        firebase.database().ref('notes').child(this.$route.params.id).update({
+          text: this.note.text,
+          timestamp: firebase.database.ServerValue.TIMESTAMP
+        })
+      }
+    },
+    destroyNote () {
+      firebase.database().ref('notes').child(this.$route.params.id).remove()
+      this.$router.push('/#')
     }
   }
 }
 </script>
 
 <style lang="sass">
-p
+.note-text
   width: 97%
   margin: auto
 
@@ -63,4 +84,11 @@ textarea
   margin-left: 1.5%
   -webkit-font-smoothing: antialiased
   -moz-osx-font-smoothing: grayscale
+
+h1{ font-size: 2em; }
+h2 { font-size: 1.5em; }
+h3 { font-size: 1.17em; }
+h4 { font-size: 1.12em; }
+h5 { font-size: .83em; }
+h6 { font-size: .75em; }
 </style>
